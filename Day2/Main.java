c3d92dfd572180d4e10238b6d0f974c472a38c91
thisizharini <nithyaharini2625@gmail.com>
2025-10-18 13:03:29 +0530

    Initial commit-Added Day 1,2,3

---
+++ Day2/Main.java
@@ -0,0 +1,18 @@
+package Day2;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int number=scanner.nextInt();
+        if(number % 2==0){
+            System.out.println("This number is Even");
+
+        }
+        else{
+            System.out.println("This number is Odd");
+        }
+        scanner.close();
+}
+    }
